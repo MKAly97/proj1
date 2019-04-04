a865d70719a49af8c20f6891d62720254c856c7c
Mohamed  Aly <c3336541@uon.edu.au>
2019-04-04 07:14:14 +0000

    code been run

--- proj1.c
+++ proj1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main () {
-    printf ("Hay Yall !!\n");
+    printf ("Hay Yall!! whats your age?\n");
+    int age = 0;
+    scanf ("%d",&age);
+    printf ("your age is %d\n", age);
     return 0;
 }
